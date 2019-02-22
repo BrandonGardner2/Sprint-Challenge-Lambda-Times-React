8e138342b124e5b9160b1cd58d3e2dc36cc4ca76
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-22 10:33:12 -0600

    All errors cleared

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Card from "./Card";
 
@@ -7,8 +7,8 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
-      {props.cards.map(card => (
-        <Card card={card} />
+      {props.cards.map((card, index) => (
+        <Card card={card} key={index * Math.floor(Math.random() * 5000) + 5} />
       ))}
     </div>
   );
